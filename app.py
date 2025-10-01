a886a7ddad17bb86e0f55cae6d89d7d4684773d7
Juansebastianlopez33 <juanselopezmora@hotmail.com>
2025-10-01 02:18:08 -0500

    cambio en app para ajustar los puertos

--- app.py
+++ app.py
@@ -213,4 +213,5 @@ def on_leave(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render asigna $PORT
+    socketio.run(app, host='0.0.0.0', port=port, debug=True, allow_unsafe_werkzeug=True)
